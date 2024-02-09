51cca73aa8a78fa708721941cad169f1281e56b9
lppduy <lephamphuongduy@gmail.com>
2024-02-09 14:11:50 +0700

    Project initial setup

---
+++ src/test/java/com/lppduy/vehiclesearch/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lppduy.vehiclesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
