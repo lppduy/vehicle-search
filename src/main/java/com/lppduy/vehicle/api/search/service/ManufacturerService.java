99717cf125f54e1abb202e6e22d1aec5aa46255e
lppduy <lephamphuongduy@gmail.com>
2024-02-10 13:48:47 +0700

    Added controller endpoint and service method to save manufacturer in DB

---
+++ src/main/java/com/lppduy/vehicle/api/search/service/ManufacturerService.java
@@ -0,0 +1,8 @@
+package com.lppduy.vehicle.api.search.service;
+
+import com.lppduy.vehicle.api.search.entity.Manufacturer;
+
+public interface ManufacturerService {
+
+    Manufacturer saveManufacturer(Manufacturer manufacturer);
+}
