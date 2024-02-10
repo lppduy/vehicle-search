50023e6cec24f836671b33d5a1b0e8a9ee9a86f5
lppduy <lephamphuongduy@gmail.com>
2024-02-10 14:14:16 +0700

    Added rest endpoint to get all my manufacturers

--- src/main/java/com/lppduy/vehicle/api/search/service/ManufacturerService.java
+++ src/main/java/com/lppduy/vehicle/api/search/service/ManufacturerService.java
@@ -2,7 +2,10 @@ package com.lppduy.vehicle.api.search.service;
 
 import com.lppduy.vehicle.api.search.entity.Manufacturer;
 
+import java.util.List;
+
 public interface ManufacturerService {
 
     Manufacturer saveManufacturer(Manufacturer manufacturer);
+    List<Manufacturer> fetchAllManufacturers();
 }
